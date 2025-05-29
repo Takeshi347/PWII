b30d518773afbd1606e1f490a79430c95f4f3c9a
Takeshi347 <mateus2908pereira@gmail.com>
2025-05-29 08:53:19 -0300

    Add files via upload

---
+++ PDO_CRUD/public/usuario/read.php
@@ -0,0 +1,24 @@
+<?php 
+    include'../../config/connection.php';
+
+    $stmt = $pdo->query('SELECT * FROM usuario');
+    $usuarios = $stmt->fetchAll();
+    
+    if( isset($_GET['id']) ) {
+        $id = $_GET['id'];
+        echo "O id $id foi removido111!!!";
+    }
+
+    // echo "<pre>";
+    // echo var_dump($usuarios);    
+?>
+    <?php foreach ($usuarios as $indice => $user) { ?>
+        <p>
+            <strong>Nome da Heroina:</strong><?php echo $user['username']; ?>
+            <br>
+            <a href="http://localhost/PWII/PDO_CRUD/public/usuario/delete.php?id=<?php echo $user["id"]; ?>">Remover</a>
+            <a href="">Editar</a>
+        </p>
+        <hr>
+    <?php } ?>
+    
